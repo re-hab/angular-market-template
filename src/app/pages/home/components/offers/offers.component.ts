afaf6389df40621687ec7a410e3d6d7d1fd86581
Rehab <rahmed@panarab-media.com>
2021-02-06 21:39:17 +0200

    feat: add offers section

--- src/app/pages/home/components/offers/offers.component.ts
+++ src/app/pages/home/components/offers/offers.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offers.component.scss']
 })
 export class OffersComponent implements OnInit {
+  categories: any[]=[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.categories = [
+      {
+        img: "https://picsum.photos/200/200?random=8",
+        description: "Juices"
+      },
+      {
+        img: "https://picsum.photos/200/200?random=9",
+        description: "Meat"
+      },
+      {
+        img: "https://picsum.photos/200/200?random=10",
+        description: "Hot Drinks"
+      },
+      {
+        img: "https://picsum.photos/200/200?random=11",
+        description: "Home Care"
+      }
+    ]
   }
 
 }
